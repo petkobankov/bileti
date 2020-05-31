e5c2d964af6d728f20e913df724388fa5d9caa69
petko.h.bankov <petko.h.bankov@gmail.com>
2020-05-31 10:09:38 +0300

    2

--- bileti/BookSeat.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Seat.h"
-class BookSeat: public Seat {
-	char* note;
-public:
-	BookSeat(int _row, int _seat, const char* _forEvent,const char* _date,int _hallId,const char* _note);
-};
--- bileti/PurchaseSeat.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Seat.h"
-class PurchaseSeat: public Seat {
-	char* ticketId;
-public:
-	PurchaseSeat(int _row, int _seat, const char* _forEvent,const char* _date, int _hallId);
-	const char* intToChar(int _a)const;
-	const int sizeOfInt(int _a)const;
-};
